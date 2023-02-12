c969e4f8fefb73a0627ff29ab430a3660e753bec
limepillX <justtedyt@yahoo.com>
2023-02-13 03:29:11 +0400

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Yakovenko_072303_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SelectAll = new System.Windows.Forms.Button();
+            this.Select1 = new System.Windows.Forms.Button();
+            this.Insert = new System.Windows.Forms.Button();
+            this.Update = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // SelectAll
+            // 
+            this.SelectAll.Location = new System.Drawing.Point(567, 226);
+            this.SelectAll.Name = "SelectAll";
+            this.SelectAll.Size = new System.Drawing.Size(75, 23);
+            this.SelectAll.TabIndex = 0;
+            this.SelectAll.Text = "SelectAll";
+            this.SelectAll.UseVisualStyleBackColor = true;
+            this.SelectAll.Click += new System.EventHandler(this.SelectAll_Click);
+            // 
+            // Select1
+            // 
+            this.Select1.Location = new System.Drawing.Point(567, 178);
+            this.Select1.Name = "Select1";
+            this.Select1.Size = new System.Drawing.Size(75, 23);
+            this.Select1.TabIndex = 1;
+            this.Select1.Text = "Select1";
+            this.Select1.UseVisualStyleBackColor = true;
+            this.Select1.Click += new System.EventHandler(this.Select1_Click);
+            // 
+            // Insert
+            // 
+            this.Insert.Location = new System.Drawing.Point(567, 276);
+            this.Insert.Name = "Insert";
+            this.Insert.Size = new System.Drawing.Size(75, 23);
+            this.Insert.TabIndex = 2;
+            this.Insert.Text = "Insert";
+            this.Insert.UseVisualStyleBackColor = true;
+            this.Insert.Click += new System.EventHandler(this.Insert_Click);
+            // 
+            // Update
+            // 
+            this.Update.Location = new System.Drawing.Point(567, 337);
+            this.Update.Name = "Update";
+            this.Update.Size = new System.Drawing.Size(75, 23);
+            this.Update.TabIndex = 3;
+            this.Update.Text = "Update";
+            this.Update.UseVisualStyleBackColor = true;
+            this.Update.Click += new System.EventHandler(this.Update_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(567, 399);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(75, 23);
+            this.Delete.TabIndex = 4;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(24, 178);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(515, 244);
+            this.listBox1.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 117);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(515, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Update);
+            this.Controls.Add(this.Insert);
+            this.Controls.Add(this.Select1);
+            this.Controls.Add(this.SelectAll);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SelectAll;
+        private System.Windows.Forms.Button Select1;
+        private System.Windows.Forms.Button Insert;
+        private System.Windows.Forms.Button Update;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+
+namespace Yakovenko_072303_5
+{
+    public partial class Form1 : Form
+    {
+        public static string connectString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=db1.mdb;";
+        // вариант 2
+        //public  static  string  connectString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Workers.mdb;";
+        // поле-ссылка на экземпляр класса OleDbConnection для соединения
+        private OleDbConnection myConnection;
+        public Form1()
+        {
+            InitializeComponent();
+            //  создаем экземпляр класса OleDbConnection
+            myConnection = new OleDbConnection(connectString);
+            // открываем соединение с БД
+            myConnection.Open();
+        }
+
+        private void Select1_Click(object sender, EventArgs e)
+        {
+            // обработчик события нажатия кнопки SELECT1 - элемент, выбранный в //listBox1, переписывается в textBox1
+
+            string element = listBox1.SelectedItem.ToString();
+            element = element.Substring(element.IndexOf(' '));   // Удалить id
+            textBox1.Text = element;
+        }
+
+        private void SelectAll_Click(object sender, EventArgs e)
+        {
+            // текст запроса
+            string query = "SELECT Code, Group, Faculty, Namee, AvMark FROM Eksamens ORDER BY Code";
+            // string query = "SELECT * FROM Eksamens";
+            // создаем объект OleDbCommand для выполнения запроса к БД MS Access
+            OleDbCommand command = new OleDbCommand(query, myConnection);
+            // получаем объект OleDbDataReader для чтения табличного результата запроса SELECT
+            OleDbDataReader reader = command.ExecuteReader();
+            // очищаем listBox1
+            listBox1.Items.Clear();
+            // в цикле построчно читаем ответ от БД
+            while (reader.Read())
+            {
+                // выводим данные столбцов текущей строки в listBox1
+                listBox1.Items.Add(reader[0].ToString() + " " + reader[1].ToString() + " " + reader[2].ToString() + " " + reader[3].ToString() + " " + reader[4].ToString());
+            }
+            // закрываем OleDbDataReader
+            reader.Close();
+
+
+        }
+
+        private void Insert_Click(object sender, EventArgs e)
+        {
+            // Получить строку из textBox1
+            string element = textBox1.Text;
+            element = element.Trim();    //Удалить начальные и конечные пробелы
+                                         //Выделить поля
+            string[] words = element.Split(new char[] { ' ' });
+            string Group = words[0];
+            string Faculty = words[1];
+            string Namee = words[2];
+            string AvMark = words[3];
+
+
+
+            // Сформировать запрос
+            string query = "INSERT INTO Eksamens ( `Group`, Faculty, Namee, AvMark) VALUES (";
+            string tmp = Group + ", ";
+            query += tmp;
+
+            tmp = "'" + Faculty + "', ";
+
+            query += tmp;
+            tmp = "'" + Namee + "', ";
+
+            query += tmp;
+            tmp = AvMark + ")";
+
+            query += tmp;
+
+
+
+
+            // создаем объект OleDbCommand для выполнения запроса к БД MS Access
+            OleDbCommand command = new OleDbCommand(query, myConnection);
+            // выполняем запрос к MS Access
+            command.ExecuteNonQuery();
+            // Отобразить новое состояние таблицы
+            // текст запроса
+            string query1 = "SELECT Code,  Group, Faculty, Namee, AvMark FROM Eksamens ORDER BY Code";
+            // создаем объект OleDbCommand для выполнения запроса к БД MS Access
+            OleDbCommand command1 = new OleDbCommand(query1, myConnection);
+            // получаем объект OleDbDataReader для чтения табличного результата запроса SELECT
+            OleDbDataReader reader = command1.ExecuteReader();
+            // очищаем listBox1
+            listBox1.Items.Clear();
+            // в цикле построчно читаем ответ от БД
+            while (reader.Read())
+            {
+                // выводим данные столбцов текущей строки в listBox1
+                listBox1.Items.Add(reader[0].ToString() + " " + reader[1].ToString() + " " + reader[2].ToString() + " " + reader[3].ToString() + " " + reader[4].ToString());
+            }
+            // закрываем OleDbDataReader
+            reader.Close();
+
+        }
+
+        private void Update_Click(object sender, EventArgs e)
+        {
+            // Определить элемент, выделенный в списке listbox
+            string element = listBox1.SelectedItem.ToString();
+            string id = element.Remove(element.IndexOf(' '));   // Выделить id
+                                                                // Получить строку из textBox1
+            element = textBox1.Text;
+            element = element.Trim();    //Удалить начальные и конечные пробелы
+                                         //Выделить поля
+            string[] words = element.Split(new char[] { ' ' });
+            string Group = words[0];
+            string Faculty = words[1];
+            string Namee = words[2];
+            string AvMark = words[3];
+
+            // Сформировать запрос
+            string query = "UPDATE Eksamens SET `Group` = ";
+            string tmp = Group + ",";
+            query += tmp;
+
+            tmp = " Faculty = '" + Faculty + "',";
+
+            query += tmp;
+            tmp = " Namee = '" + Namee + "',";
+
+            query += tmp;
+            tmp = " AvMark = " + AvMark;
+
+            query += tmp;
+            tmp = " WHERE Code = " + id;
+
+            query += tmp;
+
+            Console.WriteLine(query);
+
+            // создаем объект OleDbCommand для выполнения запроса к БД MS Access
+            OleDbCommand command = new OleDbCommand(query, myConnection);
+            // выполняем запрос к MS Access
+            command.ExecuteNonQuery();
+            // Отобразить новое состояние таблицы
+            // текст запроса
+            string query1 = "SELECT Code,  `Group`, Faculty, Namee, AvMark FROM Eksamens ORDER BY Code";
+            // создаем объект OleDbCommand для выполнения запроса к БД MS Access
+            OleDbCommand command1 = new OleDbCommand(query1, myConnection);
+            // получаем объект OleDbDataReader для чтения табличного результата запроса SELECT
+            OleDbDataReader reader = command1.ExecuteReader();
+            // очищаем listBox1
+            listBox1.Items.Clear();
+            // в цикле построчно читаем ответ от БД
+            while (reader.Read())
+            {
+                // выводим данные столбцов текущей строки в listBox1
+                listBox1.Items.Add(reader[0].ToString() + " " + reader[1].ToString() + " " + reader[2].ToString() + " " + reader[3].ToString() + " " + reader[4].ToString());
+            }
+            // закрываем OleDbDataReader
+            reader.Close();
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            // Определить элемент, выделенный в списке listbox
+            string element = listBox1.SelectedItem.ToString();
+            element = element.Remove(element.IndexOf(' '));   // Выделить id
+            string query = "DELETE FROM Eksamens WHERE Code = ";
+            query += element;
+            // создаем объект OleDbCommand для выполнения запроса к БД MS Access
+            OleDbCommand command = new OleDbCommand(query, myConnection);
+            // выполняем запрос к MS Access
+            command.ExecuteNonQuery();
+            // Отобразить новое состояние таблицы
+            string query1 = "SELECT Code,  Group, Faculty, Namee, AvMark FROM Eksamens ORDER BY Code";
+            // создаем объект OleDbCommand для выполнения запроса к БД MS Access
+            OleDbCommand command1 = new OleDbCommand(query1, myConnection);
+            // получаем объект OleDbDataReader для чтения табличного результата запроса SELECT
+            OleDbDataReader reader = command1.ExecuteReader();
+            // очищаем listBox1
+            listBox1.Items.Clear();
+            // в цикле построчно читаем ответ от БД
+            while (reader.Read())
+            {
+                // выводим данные столбцов текущей строки в listBox1
+                listBox1.Items.Add(reader[0].ToString() + " " + reader[1].ToString() + " " + reader[2].ToString() + " " + reader[3].ToString() + " " + reader[4].ToString() + " ");
+            }
+            // закрываем OleDbDataReader
+            reader.Close();
+
+        }
+        // обработчик события закрытия формы
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // закрываем соединение с БД
+            myConnection.Close();
+        }
+
+    }
+}
